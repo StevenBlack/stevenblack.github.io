7b73a06a50f703b52c279b63c09ab364911db7b3
Steven Black <steveb@stevenblack.com>
2015-01-28 20:11:32 -0500

    Cleanup the JS and CSS folder.

--- js/jquery.sbc.js
+++
@@ -1,1 +0,0 @@
-jQuery.fn.vjustify=function(){var maxHeight=0;this.each(function(){if(this.offsetHeight>maxHeight){maxHeight=this.offsetHeight;}});this.each(function(){$(this).height(maxHeight+"px");if(this.offsetHeight>maxHeight){$(this).height((maxHeight-(this.offsetHeight-maxHeight))+"px");}});};
